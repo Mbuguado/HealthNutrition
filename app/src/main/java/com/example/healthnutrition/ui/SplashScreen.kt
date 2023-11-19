57abe7ec6853a443d704ad8019b22e996f0ca029
Mbugua <chegedennis1999@gmail.com>
2023-11-19 21:56:56 +0300

    first commit

---
+++ app/src/main/java/com/example/healthnutrition/ui/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.example.healthnutrition.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.healthnutrition.R
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        //HIDE THE ACTION BAR
+        supportActionBar?.hide()
+        val windowInsetsController = WindowCompat.getInsetsController(
+            window, window.decorView
+        )
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+        //ANIMATE THE TEXT
+        val topTextView: TextView = findViewById(R.id.top_text)
+        val bottomTextView: TextView = findViewById(R.id.bottom_text)
+        val topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        topTextView.startAnimation(topAnimation)
+        bottomTextView.startAnimation(bottomAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
