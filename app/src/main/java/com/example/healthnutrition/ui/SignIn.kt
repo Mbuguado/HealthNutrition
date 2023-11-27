2802c4ccb116221017d4f720eea0816ea882db89
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-11-27 03:25:49 +0300

    handle google signin request

--- app/src/main/java/com/example/healthnutrition/ui/SignIn.kt
+++ app/src/main/java/com/example/healthnutrition/ui/SignIn.kt
@@ -1,5 +1,6 @@
 package com.example.healthnutrition.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,15 +9,20 @@ import android.widget.Toast
 import com.example.healthnutrition.Constants.Constants
 import com.example.healthnutrition.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
+@Suppress("DEPRECATION")
 class SignIn : AppCompatActivity() {
 
     lateinit var googleSignInClient: GoogleSignInClient
-    val requestCode = Constants.REQ_CODE
+    val reqCode = Constants.REQ_CODE
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var signIn: Button
 
@@ -42,6 +48,31 @@ class SignIn : AppCompatActivity() {
     }
 
     private fun signInGoogle() {
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, reqCode)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == reqCode) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                updateUi(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUi(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.id, null)
     }
 }
