b20f440811562d7598886fed8df4edbd5baeb9c7
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-01-22 21:28:06 +0300

    Authentication screens UI

---
+++ app/src/main/java/com/example/healthnutrition/auth/SignUp.kt
@@ -0,0 +1,12 @@
+package com.example.healthnutrition.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healthnutrition.R
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
