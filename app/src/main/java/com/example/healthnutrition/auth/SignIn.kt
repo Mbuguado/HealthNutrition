3c4f309a8a80e3700776e049b13abde47a24dc17
Alvin Mwakai <mwakaimwambala@gmail.com>
2024-01-23 13:40:41 +0300

    button to the main activity

--- app/src/main/java/com/example/healthnutrition/auth/SignIn.kt
+++ app/src/main/java/com/example/healthnutrition/auth/SignIn.kt
@@ -17,79 +17,22 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
-@Suppress("DEPRECATION")
 class SignIn : AppCompatActivity() {
 
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private val reqCode = Constants.REQ_CODE
-    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var signIn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        //INITIALIZE FIREBASEAUTH
-        FirebaseApp.initializeApp(this)
-        signIn = findViewById(R.id.login)
 
-        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOption)
-        firebaseAuth = FirebaseAuth.getInstance()
 
         signIn.setOnClickListener {
             Toast.makeText(this, "Logging In", Toast.LENGTH_SHORT).show()
-            signInGoogle()
-        }
-
-    }
-
-    private fun signInGoogle() {
-        val signInIntent: Intent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, reqCode)
-
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == reqCode) {
-            try {
-                val task: GoogleSignInAccount? = GoogleSignIn
-                    .getSignedInAccountFromIntent(data)
-                    .getResult(ApiException::class.java)
-                if (task != null) {
-                    handleSignInResult(task)
-                }
-            } catch (e: ApiException) {
-                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun handleSignInResult(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (firebaseAuth.currentUser != null) {
-            val intent = Intent(applicationContext, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+
     }
 
 }
